7c060235fe6cdf959631d82d7ad34c7e06a7e59f
L13 <L13.RARY@gmail.com>
2021-06-03 21:43:26 +0200

    - updated package.json and build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 //	Imports ____________________________________________________________________
 
+const child_process = require('child_process');
 const del = require('del');
+const fs = require('fs');
+const glob = require('glob');
 const gulp = require('gulp');
+const { ESLint } = require('eslint');
 const rollup = require('rollup');
 
 const typescript = require('@rollup/plugin-typescript');
@@ -18,7 +22,7 @@ const typescript = require('@rollup/plugin-typescript');
 
 gulp.task('clean', () => {
 	
-	return del(['out']);
+	return del(['out', 'test']);
 	
 });
 
@@ -29,7 +33,7 @@ gulp.task('copy', () => {
 
 });
 
-gulp.task('script', () => {
+gulp.task('script:extension', () => {
 	
 	return rollup.rollup({
 		input: 'src/extension.ts',
@@ -42,14 +46,14 @@ gulp.task('script', () => {
 		plugins: [
 			typescript({
 				include: [
-					'src/**/*.ts',
+					'src/**/!(*.test).ts',
 				],
 			}),
 		]
 	}).then(bundle => {
 		
 		return bundle.write({
-			file: './out/extension.js',
+			file: 'out/extension.js',
 			format: 'cjs',
 			globals: {
 				fs: 'fs',
@@ -62,19 +66,107 @@ gulp.task('script', () => {
 	
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'script'));
+gulp.task('script:tests', () => {
+	
+	const promises = [];
+	
+	[{ in: 'src/test/index.ts', out: 'test/index.js'}]
+	.concat(createInOut('src/**/*.test.ts'))
+	.forEach((file) => {
+		
+		promises.push(rollup.rollup({
+			input: file.in,
+			treeshake: false,
+			onwarn,
+			external: [
+				'assert',
+				'glob',
+				'fs',
+				'mocha',
+				'path',
+			],
+			plugins: [
+				typescript({
+					include: [
+						'src/@l13/**/*.ts',
+						'src/test/index.ts',
+					],
+				}),
+			]
+		}).then(bundle => {
+			
+			return bundle.write({
+				file: file.out,
+				format: 'cjs',
+				globals: {
+					assert: 'assert',
+					glob: 'glob',
+					fs: 'fs',
+					mocha: 'mocha',
+					path: 'path',
+				},
+			});
+			
+		}, onerror));
+		
+	});
+	
+	return Promise.all(promises);
+	
+});
+
+gulp.task('lint', async (done) => {
+	
+	const eslint = new ESLint();
+	const results = await eslint.lintFiles(['src/**/*.ts']);
+	const formatter = await eslint.loadFormatter('stylish');
+	const resultText = formatter.format(results);
+	
+	if (resultText) console.log(resultText);
+	
+	done();
+	
+});
+
+gulp.task('test', (done) => {
+	
+	const tests = child_process.spawn('npm', ['test']).on('close', () => done());
+	
+	let logger = (buffer) => buffer.toString().split(/\n/).forEach((message) => message && console.log(message));
+	
+	tests.stdout.on('data', logger);
+	tests.stderr.on('data', logger);
+	
+});
+
+gulp.task('script', gulp.series('script:extension', 'script:tests'));
+
+gulp.task('build', gulp.series('clean', 'copy', 'script', 'lint', 'test'));
 
 gulp.task('watch', () => {
 	
 	gulp.watch(['src/**/*.json'], gulp.parallel('copy'));
 	
-	gulp.watch(['src/*.ts'], gulp.parallel('script'));
+	gulp.watch(['src/**/!(*.test).ts'], gulp.parallel('script'));
 	
 });
 
 gulp.task('build & watch', gulp.series('build', 'watch'));
 
 //	Functions __________________________________________________________________
+
+function createInOut (pattern) {
+	
+	return glob.sync(pattern).map((filename) => {
+		
+		return {
+			in: filename,
+			out: filename.replace(/^src/, 'test').replace(/\.ts$/, '.js'),
+		};
+		
+	});
+	
+}
 
 function onwarn (warning) {
 	
